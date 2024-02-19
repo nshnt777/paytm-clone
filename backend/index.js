b89015e3a5a60f1205a213067912d74356869a0b
nshnt777 <ns227758@gmail.com>
2024-02-20 02:15:22 +0530

    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import router from './routes/index.js'
 import cors from 'cors'
 
 const app = express();
-const port = process.env.POST || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 //disable cors errors
